b80eef6bfa5fb931a8eec47e7efc2423e844e2e5
tim99919 <tim99919@gmail.com>
2019-04-16 00:36:53 +0300

    learnjs-react-screencast Lesson 1-8 done

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ArticleList from './ArticleList';
+import articles from '../fixtures';
+import 'bootstrap/dist/css/bootstrap.css'
+
+function App() {
+  return (
+    <div className="container">
+      <div className="jumbotron">
+        <h1 className="display-3">App name</h1>
+      </div>
+      <ArticleList articles={articles} />
+    </div>
+  )
+}
+
+export default App
